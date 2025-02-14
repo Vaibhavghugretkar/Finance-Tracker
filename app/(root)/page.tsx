9ed302d6d80196fd84a4e90c31af8dbb0013a391
SunidhiH <vaibhavghugretkar23@gmail.com>
2025-02-14 16:08:55 +0530

    built Home-UI

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,37 @@
+import HeaderBox from '@/components/HeaderBox'
+import RightSidebar from '@/components/RightSideBar'
+import TotalBalanceBox from '@/components/TotalBalanceBox'
 import React from 'react'
 
 const Home = () => {
+  const loggedIn ={firstName:"Vaibhav"}
   return (
-    <div>Home</div>
+    <section className='home'>
+      <div className='home-content'>
+        <header className='home-header'>
+          <HeaderBox
+          type="greeting"
+          title="Welcome"
+          user={loggedIn?.firstName || 'Guest'}
+          subtext="Access and manage your account and transactions efficiently."
+          />
+         <TotalBalanceBox 
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.30}
+          />
+        </header>
+        RECENT TRANSACTIONS
+      </div>
+
+      <RightSidebar
+        user={loggedIn}
+        transactions={[]}
+        banks={[{currentBalance:1235.50},{}]}
+
+      />
+    </section>
   )
 }
 
-export default Home
+export default Home 
