f3e3113a1a69e9c2b08f649443fa72d6264cfd5e
SunidhiH <vaibhavghugretkar23@gmail.com>
2025-02-16 20:10:26 +0530

    auth-backend

---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://a5f19ff9817e2dc083a578d6ca0ead4d@o4508829274931200.ingest.us.sentry.io/4508829276962816",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
